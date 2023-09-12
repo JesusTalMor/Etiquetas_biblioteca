50596c69e4c299e39d10619887c106aed79ae472
Jesus Talamantes <a01706335@itesm.mx>
2023-09-12 14:59:25 -0600

    Creador de Base de Datos

--- ticket_maker.py
+++ ticket_maker.py
@@ -6,6 +6,7 @@ from fpdf import FPDF
 from PIL import Image, ImageDraw, ImageFont
 
 import pop_ups as pop
+from managers import Etiqueta, Libro
 
 
 # TODO Ajustar el encabezado 1 cm de margen, listo pero no me convence
@@ -43,7 +44,6 @@ sg.theme('MyCreatedTheme')
 
 
 class TicketMaker:
-
   """ Clase para la creacion de Etiquetas 
   
     Puede crear paginas completas de etiquetas o una base de datos
@@ -260,8 +260,65 @@ class TicketMaker:
       ticket_database.write('\n')
     ticket_database.close()
 
+class DatabaseMaker:
+  """ Clase implementada para crear bases de datos con txt tipo csv """
+  def crear_database(self, aListaLibros:list, aNombre:str, aRuta:str):
+    if len(aListaLibros) == 0: return # Revisar que tenemos datos
+    txt_path = f'{aRuta}/{aNombre}_etiquetas.txt'
+    database_writer = open(txt_path, 'w', encoding="utf-8")
+    for libro in aListaLibros:
+      #* Crear lista para base de datos
+      lista_etiqueta = self.separar_lista(libro.etiqueta)
+      limpiar_lista = [x for ind, x in enumerate(lista_etiqueta) if x != '' or ind == 0]
+      #* Recorrer todos los elementos de la etiqueta.
+      for elem in limpiar_lista: database_writer.write(f'{elem},')
+      database_writer.write('\n')
+    database_writer.close()
 
-
+  def separar_lista(self, aEtiqueta:Etiqueta):
+    """ Recibe un diccionario y crea una lista para imprimir """
+    lista_salida = []
+    #* Usando un diccionario obtener todos los elementos
+    encabezado = aEtiqueta.encabezado
+    clasif = self.separar_clasificacion(aEtiqueta.clasif)
+    volumen = 'V.' + aEtiqueta.volumen
+    copia = 'C.' + aEtiqueta.copia
+    #* Juntar todos los elementos
+    lista_salida.extend(encabezado)
+    lista_salida.extend(clasif)
+    lista_salida.extend(volumen)
+    lista_salida.extend(copia)
+    #* Limpiar elementos vacios
+    # lista_salida = [x for x in lista_salida if x != '']
+    # print('Entrada', str_dict, sep='\n')
+    # print('Salida', lista_salida, sep='\n')
+    return lista_salida
+  def separar_clasificacion(self, STR:str):
+    """ Separa un Clasificación y retorna una lista"""
+    lista_salida = []
+    # * Separar por espacios separa PIPE A de PIPE B y la anterior en autor y año
+    space_list = STR.split(' ')  
+    # Ejemplo 'PQ7298.424.A76 .O744 2007' -> [PQ7298.424.A76] [.O744] [2007]
+    #* Separa el PIPE A en sus atributos: Categorias XD
+    pipe_a = space_list.pop(0) 
+    # Ejemplo [PQ7298.424.A76] [.O744] [2007] -> [PQ7298.424.A76] | [.O744] [2007]
+    pipe_a = pipe_a.split('.') 
+    # Ejemplo [PQ7298.424.A76] -> [PQ7298] [424] [A76]
+    letras_tema = pipe_a.pop(0)
+    # Ejemplo [PQ7298] [424] [A76] -> [PQ7298] | [424] [A76]
+    # print(space_list, pipe_a, letras_tema, sep='\n')
+    #* Trabajar en separar letras y numeros
+    letras_tema = [letras_tema[:2], letras_tema[2:]] if letras_tema[1] in alphabet else [letras_tema[:1], letras_tema[1:]]
+    # Ejemplo PQ7298 -> [PQ, 7298] si 2 letras otro caso P7298 -> [P, 7298]
+    # print(space_list, pipe_a, letras_tema, sep='\n')
+    #* Juntar todas las listas en la salida
+    lista_salida.extend(letras_tema)
+    lista_salida.extend(pipe_a)
+    lista_salida.extend(space_list)
+    # Ejemplo [PQ, 7298] + [424, A76] + [.O744, 2007] -> ['PQ', '7298', '424', 'A76', '.O744', '2007']
+    # print('Entrada', STR, sep='\n')
+    # print('Salida Final', lista_salida, sep='\n')
+    return lista_salida
 
 def image_viewer(lista_de_rutas: list, flag: bool):
   #* Estandarizar tamaño de la imagen
