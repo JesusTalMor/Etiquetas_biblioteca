b546acdfebc4dbe4e26aaa4d578a1b0c56ef6db7
Jesus Talamantes <a01706335@itesm.mx>
2023-12-02 22:43:32 -0600

    Actualizar estructura de los pop ups

--- graphical_interface.py
+++ graphical_interface.py
@@ -739,3 +739,4 @@ def main():
 
 if __name__ == '__main__':
   main()
+  pop.check_pdf()
--- pop_ups.py
+++ pop_ups.py
@@ -1,68 +1,108 @@
-from tkinter import messagebox
+import os
+import sys
+from tkinter import Tk, messagebox
 
 
-# # ? POP_up de advertencia (Warning)
-##########################################################
-def warning_select():
-  msg = f'!Advertencia!\nSeleccione elementos a imprimir'
-  messagebox.showwarning("No Selected", msg)
+#?#********** Función apoyo para relative path *********#
+def resource_path(relative_path):
+  """ Get absolute path to resource, works for dev and for PyInstaller """
+  try:
+    # PyInstaller creates a temp folder and stores path in _MEIPASS
+    base_path = sys._MEIPASS
+  except Exception:
+    base_path = os.path.abspath(".")
+  return os.path.join(base_path, relative_path)
 
-def warning_excel_file():
-  msg = f'!Fallo!\nSeleccione un archivo de Excel'
-  messagebox.showwarning("No Excel File", msg)
+window = Tk()
+window.iconbitmap(resource_path('Assets/ticket_icon.ico'))
+window.withdraw()
 
-def warning_excel_file_data_error():
-  msg = f'!Advertencia!\n Algunas Etiquetas no logran cargarse'
-  messagebox.showwarning("File Data Warning", msg)
+# ? POP_up de advertencia (Warning)
+def warning_pop(option:str):
+  """ Generar un pop up de Advertencia
+    Opciones
+    ----------
+    selection : No se seleccionaron elementos de etiquetas
+    no_file   : No se tiene un archivo de excel para usar
+  """
+  posible_options = {
+    'selection' : (
+      'No selection',
+      f'!Advertencia!\n Seleccione elementos a imprimir'
+    ),
+    'no_file' : (
+      'No Excel File',
+      f'!Fallo!\n Selecione un archivo de Excel'
+    ),
+  }
+  title, msg = posible_options[option]
+  messagebox.showwarning(title=title, message=msg, parent=window)
 
-def warning_folder():
-  msg = f'!Fallo!\nSeleccione una carpeta de Salida'
-  messagebox.showwarning("No Folder", msg)
-##########################################################
-
-# # ? up de Error (Error)
-##########################################################
-def error_excel_file():
-  msg = f'!Fallo!\n Ninguna Etiqueta se cargo'
-  messagebox.showerror("Excel File Error", msg)
-##########################################################
-
-# # ? up de Informacion (Info)
-##########################################################
-def success_program():
-  msg = f'Felicidades\n El programa finalizó con exito'
-  messagebox.showinfo("Finalize", msg)
-
-def info_license():
-  msg = f'Programa Bajo Licencia del Tecnológico de Monterrey\n !No reproducir sin permiso!'
-  messagebox.showinfo("Licencia", msg)
-
-def info_about(version):
-  msg = f'Programa Generador de Etiquetas\n Versión:{version}\n Por: Jesus Talamantes Morales 2023'
-  messagebox.showinfo("Licencia", msg)
-
-def show_info_libro(titulo:str):
-  msg = f'Título del Libro:\n {titulo}'
-  messagebox.showinfo("Book Info", msg)
+def error_pop(option:str):
+  """ Genera un pop up de error 
+    Opciones
+    ---------
+    excel_file : El archivo de excel cuenta con errores graves
+  """
+  posible_options = {
+    'excel_file' : (
+      'Excel File Error',
+      f'!Fallo!\n Ninguna Etiqueta se cargo'
+    ),
+  }
+  title, msg = posible_options[option]
+  messagebox.showerror(title=title, message=msg, parent=window)
 
 
-# # ? up de OkCancel Ask (askokcancel)
-##########################################################
-def check_pdf():
-  msg = f'Hojas Generadas\n Presione Aceptar para continuar con su PDF'
-  answer = messagebox.askokcancel("Confirmar", msg)
-  return answer
-def check_images():
-  msg = f'Imágenes Prueba Generadas\n Presione Aceptar para generar todas las etiquetas'
-  answer = messagebox.askokcancel("Confirmar", msg)
-  return answer
-def save_file():
-  msg = f'Desea guardar su progreso ?'
-  answer = messagebox.askokcancel("Save Progress", msg)
-  return answer
+def info_pop(option:str, aditional=''):
+  """ Genera un pop up de informacion 
+    Opciones
+    ---------
+    success   : El programa finalizo con exito
+    license   : Informacion en licencia de la aplicacion
+    about     : Informacion del programa en general
+    book_info : Informacion adicional del libro
+  """
+  posible_options = {
+    'success' : (
+      'Program Success',
+      f'!Felicidades!\n El programa finalizó con éxito'
+    ),
+    'license' : (
+      'Program License',
+      f'Programa bajo uso de Licencia.\nTecnológico de Monterrey {aditional}\n!No reproducir sin permiso!'
+    ),
+    'about' : (
+      'About Program',
+      f'Manejador de Etiquetas.\nVersión: {aditional}\n Por: Jesus Talamantes Morales\n CEL: 442 343 6407 ©2022-2023'
+    ),
+    'book_info': (
+      'Book Aditional Info',
+      f'Título del Libro: {aditional[0]}\n Código de Barras: {aditional[1]}'
+    )
+
+  }
+  title, msg = posible_options[option]
+  messagebox.showerror(title=title, message=msg, parent=window)
+
+def ask_pop(option:str):
+  """ Genera un pop up de pregunta  
+    Opciones
+    ----------
+  """
+  posible_options = {
+    'save' : (
+      'Save Program Progress',
+      f'Desea guardar su progreso ?'
+    ),
+  }
+  title, msg = posible_options[option]
+  return messagebox.askokcancel(title=title, message=msg, parent=window)
+
 
 if __name__ == "__main__":
-  print(check_images())
+  pass
+  
 
 
 
